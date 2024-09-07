9a4e7e2d78d67a846df88d016c230c219c48bf52
Viktor Todosijevic <todosijevicviktor998@gmail.com>
2024-09-07 16:58:08 +0200

    test costs

---
+++ tests/test_costs.py
@@ -0,0 +1,39 @@
+from typing import List
+from unittest import TestCase
+
+import torch
+from omegaconf import DictConfig
+from torch_geometric.loader import DataLoader
+
+from opf_dataset_utils.costs import calculate_costs_per_grid
+from tests.utils import setup_test
+
+
+class TestCosts(TestCase):
+    cfg: DictConfig
+    device: torch.device
+    loaders: List[DataLoader]
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Initialize test. Load config and test data.
+        Returns
+        -------
+
+        """
+        setup_test(cls)
+
+    def test_costs(self):
+        """
+        Check if value of the objective function that comes with the solved samples is equal to the one calculated
+        from the powers and cost coefficients.
+        Returns
+        -------
+
+        """
+
+        for loader in self.loaders:
+            for batch in loader:
+                error = calculate_costs_per_grid(batch, batch.y_dict) - batch.objective
+                self.assertLess(error.abs().sum(), self.cfg.cost_tolerance)
